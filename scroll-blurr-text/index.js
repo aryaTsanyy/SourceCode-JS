9b4951ea6db066a7fc15d0f5256a18fce6d311a4
aryaTsanyy <aryatsanyy@gmail.com>
2024-09-24 13:20:01 +0700

    new

--- scroll-blurr-text/index.js
+++ scroll-blurr-text/index.js
@@ -1,8 +1,4 @@
 /** @format */
-/* GSAP */
-import { gsap } from "https://cdn.jsdelivr.net/npm/gsap@3.12.5/dist/gsap.min.js";
-
-/* GSAP */
 /* Debounce */
 const debounce = (func, delay) => {
   let timerId; // Holds a reference to the timeout between calls.
