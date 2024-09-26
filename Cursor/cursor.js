4ac5bbb9ad125848f0e82ef57fb2b3d35ecb21ba
aryaTsanyy <aryatsanyy@gmail.com>
2024-09-26 14:23:05 +0700

    new

--- Cursor/cursor.js
+++ Cursor/cursor.js
@@ -1,25 +1,35 @@
 /** @format */
+
 document.addEventListener("DOMContentLoaded", () => {
-  const herosection = document.querySelector(".Hero-Section");
-  const cursor = document.createElement("div");
-  cursor.className = "cursor";
-  document.body.appendChild(cursor);
+  const section = document.querySelector(".animated-cursor-section");
+  if (section) {
+    const cursor = document.createElement("div");
+    cursor.className = "animated-cursor";
+    document.body.appendChild(cursor);
 
-  const triggerArea = herosection.querySelector(".bubble-wrapper");
+    const hero = section.querySelector(".hero");
+    if (hero) {
+      const bubbleSection = hero.querySelector(".bubble-section");
+      if (bubbleSection) {
+        const triggerArea = bubbleSection.querySelector(".bubble-wrapper");
+        if (triggerArea) {
+          section.addEventListener("mousemove", (e) => {
+            const isOverTriggerArea = isMouseOverElement(e.clientX, e.clientY, triggerArea);
+            if (isOverTriggerArea) {
+              cursor.classList.add("large-cursor");
+            } else {
+              cursor.classList.remove("large-cursor");
+            }
+            cursor.style.top = `${e.clientY}px`;
+            cursor.style.left = `${e.clientX}px`;
+          });
 
-  section.addEventListener("mousemove", (e) => {
-    const isOverTriggerArea = isMouseOverElement(e.clientX, e.clientY, triggerArea);
-    if (isOverTriggerArea) {
-      cursor.classList.add("large-cursor");
-    } else {
-      cursor.classList.remove("large-cursor");
+          function isMouseOverElement(x, y, element) {
+            const rect = element.getBoundingClientRect();
+            return x >= rect.left && x <= rect.right && y >= rect.top && y <= rect.bottom;
+          }
+        }
+      }
     }
-    cursor.style.top = `${e.clientY}px`;
-    cursor.style.left = `${e.clientX}px`;
-  });
-
-  function isMouseOverElement(x, y, element) {
-    const rect = element.getBoundingClientRect();
-    return x >= rect.left && x <= rect.right && y >= rect.top && y <= rect.bottom;
   }
 });
