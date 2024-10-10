80483b12cb2bfdd2d62b04e35bf9e321d66d52a2
aryaTsanyy <aryatsanyy@gmail.com>
2024-10-10 10:48:08 +0700

    new

--- Cursor/cursor-menu.js
+++ Cursor/cursor-menu.js
@@ -1,7 +1,7 @@
 /** @format */
 
 (function () {
-  const link = document.querySelectorAll(".navbar > .nav-menu");
+  const link = document.querySelectorAll(".menu > .nav-menu");
   const cursormenu = document.querySelector(".cursor-menu");
   const animateit = function (e) {
     const text = this.querySelector(".text-menu");
@@ -12,14 +12,14 @@
       yMove = (y / height) * (move * 2) - move;
 
     text.style.transform = `translate(${xMove}px, ${yMove}px)`;
-
-    const editCursor = (e) => {
-      const { clientX: x, clientY: y } = e;
-      cursormenu.style.left = x + "px";
-      cursormenu.style.top = y + "px";
-    };
-    link.forEach((b) => b.addEventListener("mousemove", animateit));
-    link.forEach((b) => b.addEventListener("mouseleave", animateit));
-    window.addEventListener("mousemove", editCursor);
+    if (e.type === "mouseleave") text.style.transform = "";
   };
+  const editCursor = (e) => {
+    const { clientX: x, clientY: y } = e;
+    cursormenu.style.left = x + "px";
+    cursormenu.style.top = y + "px";
+  };
+  link.forEach((b) => b.addEventListener("mousemove", animateit));
+  link.forEach((b) => b.addEventListener("mouseleave", animateit));
+  window.addEventListener("mousemove", editCursor);
 })();
