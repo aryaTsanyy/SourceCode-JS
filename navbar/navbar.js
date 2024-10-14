f8438d07c0aaaad1584aba22fe5267176827e30f
aryaTsanyy <aryatsanyy@gmail.com>
2024-10-15 00:59:04 +0700

    new

--- navbar/navbar.js
+++ navbar/navbar.js
@@ -1,6 +1,6 @@
 /** @format */
 
-const navbar = document.querySelector(".NavigationBar");
+const navbar = document.querySelector(".navigationbar");
 let timeoutId = null;
 
 window.addEventListener("scroll", () => {
